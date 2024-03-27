b5d1d302cb531f3a09837b01b6055706460a09b7
entrnickvana <nick.jwl123@gmail.com>
2024-03-26 21:52:25 -0600

    adding input and output functionality

--- modem.cpp
+++ modem.cpp
@@ -1,12 +1,69 @@
 
 #include "modem.h"
 #include <cstdio>
+#include <vector>
+#include <cstdlib>
 
 using namespace std;
 
 int main(){
   Modem* mdm = new Modem(10e6, 1024);
-  printf("Data Rate: %f\nFrame Size: %i", mdm->dr, mdm->fm_sz);
+  printf("Data Rate: %d\nFrame Size: %i", mdm->dr, mdm->fm_sz);
 
+  //vector<double> v{0.0, 0.707, 1.0, 0.707, 0.0, -0.707, -1.0, -0.707, -0.0};
+  
+
+  string in_name = "in_data.txt";
+  string out_name = "out_data.txt";
+  vector<double>* data_to_process = new vector<double>();
+  mdm->r_csv(in_name, data_to_process);
+  
+  //mdm->w_csv(&v, out_name);
+  //mdm->w_csv(&data_to_process, out_name);
+  mdm->w_csv(out_name);    
+  
   delete mdm;
 }
+
+
+/*
+include <iostream>
+#include <fstream>
+int main( int argc, char* argv[] )
+{
+      std::ofstream myfile;
+      myfile.open ("example.csv");
+      myfile << "This is the first cell in the first column.\n";
+      myfile << "a,b,c,\n";
+      myfile << "c,s,v,\n";
+      myfile << "1,2,3.456\n";
+      myfile << "semi;colon";
+      myfile.close();
+      return 0;
+}
+*/
+
+/*
+trackBarFile.open("TrackBarSignal.csv");
+
+std::ofstream fout;
+fout.open("Output_ReadInCSV.txt");
+
+std::string line, line2, line3;
+int count;
+float tokenNum,lineFloat,line2Float,line3Float;
+
+while (getline(trackBarFile, line,',')&&getline(trackBarFile, line2,',')&&getline(trackBarFile, line3)) 
+{
+
+    lineFloat = (float)atof(line.c_str());
+    line2Float = (float)atof(line2.c_str());
+    line3Float = (float)atof(line3.c_str());
+
+    std::cout<<line<<","<<lineFloat+2<<"   ,   "<<line2<<","<<line2Float+2<<"  ,  "<<line3<<","<<line3Float+2<<std::endl;
+
+}
+
+trackBarFile.close();
+
+*/
--- modem.h
+++ modem.h
@@ -3,8 +3,10 @@
 #include <iostream>
 //#include <boost/thread.hpp>
 #include <cmath>
-
-//using namespace std;
+#include <fstream>
+#include <vector>
+#include <string>
+using namespace std;
 
 class Modem{
 
@@ -12,10 +14,48 @@ class Modem{
   int input_mode;
   int dr;
   int fm_sz;
+  vector<double>* in_buff_ptr;
   
   Modem(double data_rate, int frame_size){
     dr = data_rate;
     fm_sz = frame_size;
+  }
+
+
+  bool w_csv(std::string& out_file_name){
+    w_csv(this->in_buff_ptr, out_file_name);
+    return true;
+  }
+
+    
+  bool w_csv(std::vector<double>* src_buff,  std::string& out_file_name){
+    std::ofstream out_file;
+    out_file.open(out_file_name);
+    
+    for(int ii=0; ii<src_buff->size(); ii++){
+      //out_file << "ii: " << ii << ", " << (*src_buff)[ii] << std::endl;
+      out_file << (*src_buff)[ii] << std::endl;      
+    }
+
+    out_file.close();
+    return true;
+  }
+
+  bool r_csv(std::string& in_file_name, std::vector<double>* dst_buff){
+    std::ifstream in_file;
+    in_file.open(in_file_name);
+
+    float lineFloat;
+    std::string line;
+    while(std::getline(in_file, line)){
+      lineFloat = (float)atof(line.c_str());
+      dst_buff->push_back(lineFloat);
+    }
+      
+    in_file.close();
+
+    this->in_buff_ptr = dst_buff;
+    return true;
   }
   
 };
@@ -29,3 +69,5 @@ class PrbsGen{
 class Fir{
  public:
 };
+
+
